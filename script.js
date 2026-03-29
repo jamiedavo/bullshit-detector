7c07fa3f1891851cde73568a9f9b9e1623e41c32
jamiedavo <jamiefrancisdavidson@gmail.com>
2026-03-29 19:59:30 +1300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Simple function to log when someone accesses an "Internal Leak"
-document.querySelectorAll('.issue-link').forEach(link => {
-    link.addEventListener('click', () => {
-        console.log("Accessing Restricted File: " + link.innerText);
-    });
-});
